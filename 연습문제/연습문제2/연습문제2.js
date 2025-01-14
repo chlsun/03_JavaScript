ccf08bea9c23e113f365b82fff959eb890c61883
chlsun <k325004@gmail.com>
2025-01-14 09:24:31 +0900

    연습문제 수정

--- 연습문제/연습문제2/연습문제2.js
+++ 연습문제/연습문제2/연습문제2.js
@@ -5,6 +5,11 @@ const result = document.querySelector("#result");
 
 for(let box of boxes){
   box.addEventListener("click", (e)=>{
+
+    if(result.textContent.length  >= 10){
+      alert("10자 까지만 입력할 수 있습니다");
+      return;
+    }
     result.append(Number(e.target.innerText));
   })
 }
